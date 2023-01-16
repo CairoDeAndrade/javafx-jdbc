4f4ebbeac429020cf91607c858fd37ad5cb432f8
Cairo Augusto de Andrade <cairo.andrade777@gmail.com>
2023-01-16 10:18:57 -0300

    Saving a new Department

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,18 +3,25 @@
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import db.DbException;
+import gui.util.Alerts;
 import gui.util.Constraints;
+import gui.util.Utils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable{
 	
 	private Department entity;
+	
+	private DepartmentService service;
 	
 	@FXML
 	private TextField textId;
@@ -35,14 +42,42 @@ public class DepartmentFormController implements Initializable{
 		this.entity = entity;
 	}
 	
-	@FXML
-	public void onBtSaveAction(ActionEvent event) {
-		System.out.println("onBtSaveAction");
+	public void SetDepartmentService(DepartmentService service) {
+		this.service = service;
 	}
 	
 	@FXML
+	public void onBtSaveAction(ActionEvent event) {
+		if (entity == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		if (service == null) {
+			throw new IllegalStateException("Service was null");
+		}
+		
+		try {
+			entity = getFormData();
+			service.saveOrUpdate(entity);
+			
+			Utils.currentStage(event).close();
+		}
+		catch (DbException e) {
+			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
+		}
+	}
+	
+	private Department getFormData() {
+		Department obj = new Department();
+		
+		obj.setId(Utils.tryParseToInt(textId.getText()));
+		obj.setName(textName.getText());
+		
+		return obj;
+	}
+
+	@FXML
 	public void onBtCancelAction(ActionEvent event) {
-		System.out.println("onBtCancelAction");
+		Utils.currentStage(event).close();
 	}
 	
 	@Override
--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -85,6 +85,7 @@ public class DepartmentListController implements Initializable{
 			
 			DepartmentFormController controller = loader.getController();
 			controller.setDepartment(obj);
+			controller.SetDepartmentService(new DepartmentService());
 			controller.updateFormData();
 			
 			Stage dialogStage = new Stage();
